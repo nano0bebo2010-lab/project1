7aae075850103b8ec74ff787ccd0966188e0217f
nano0bebo2010-lab <nano0bebo2010@gmail.com>
2026-01-04 19:24:41 +0300

    Add files via upload

---
+++ 5th/js/index.js
@@ -0,0 +1,19 @@
+// function helloWorld(){
+//     document.write("Hello world")
+    
+// }
+
+function age(age){
+    document.write(age*365*24 + "hours")
+}
+age(10.5)
+
+
+// function product(price , discount){
+//     let p = price - discount;
+
+
+//     return p;
+// }
+// var z = product(12,5)
+// document.write()
